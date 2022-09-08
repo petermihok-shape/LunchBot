8ec4f42dfd3d9af3c996223d76ca70d7cb46976c
petermihok-shape <peter.mihok@shape.dk>
2022-09-08 10:52:54 +0200

    Initial Commit

---
+++ LunchBot/LunchBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunchBotApp.swift
+//  LunchBot
+//
+//  Created by Peter Mihók on 08/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct LunchBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
